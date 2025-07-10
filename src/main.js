d2869ea69bea4528e08b0fcbecda43df791e6a71
dev <devsereti@gmail.com>
2025-07-10 23:43:24 +0300

    Errors fixed.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import router from "./router";
+import { createStore } from "vuex"; // Or your store import
+import Notifications from "@kyvg/vue3-notification"; // Vue 3 alternative
+import "./assets/css/main.css";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+const store = createStore({ /* your store config */ });
+
+app.use(router);
+app.use(store);
+app.use(Notifications);
+
+app.mount("#app");
